4678c33c80086c032282c9ef57331ca644291827
danielev1976 <daniellevin1976@gmail.com>
2026-03-01 15:37:24 +0100

    First Commit

---
+++ src/test/java/io/education/thief_catcher/ThiefCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.education.thief_catcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiefCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
